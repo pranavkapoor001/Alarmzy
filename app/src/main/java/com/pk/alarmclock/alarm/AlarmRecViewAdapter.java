aebf71581ba3867d20876c2343dfedc85aa1f5d6
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-10-09 23:29:12 +0530

    AlarmClock: Move AlarmRecyclerViewHolder to seperate class
    * Also move all onClickListeners from onBindViewHolder to ViewHolder
    * Since onBindViewHolder is called everytime view is added / data is updated
      i.e: whenever we bind view with data
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- app/src/main/java/com/pk/alarmclock/alarm/AlarmRecViewAdapter.java
+++ app/src/main/java/com/pk/alarmclock/alarm/AlarmRecViewAdapter.java
@@ -4,29 +4,21 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.SwitchCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.pk.alarmclock.R;
 import com.pk.alarmclock.alarm.db.AlarmEntity;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 
-public class AlarmRecViewAdapter extends RecyclerView.Adapter<AlarmRecViewAdapter.AlarmViewHolder> {
+public class AlarmRecViewAdapter extends RecyclerView.Adapter<AlarmRecViewHolder> {
 
     private static String TAG = "PK:AlarmRecViewAdapter";
     List<AlarmEntity> mAlarmDataList = new ArrayList<>();
-    private Context context;
 
     public void setAlarms(List<AlarmEntity> alarmDataList) {
         this.mAlarmDataList = alarmDataList;
@@ -41,64 +33,18 @@ public class AlarmRecViewAdapter extends RecyclerView.Adapter<AlarmRecViewAdapte
 
     @NonNull
     @Override
-    public AlarmViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public AlarmRecViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(
                 R.layout.alarm_item, parent, false);
-        AlarmViewHolder vh = new AlarmViewHolder(itemView);
-        context = parent.getContext();
-        return vh;
+        return new AlarmRecViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final AlarmViewHolder holder, final int position) {
-        // Get hold of currentItem in ArrayData class
+    public void onBindViewHolder(@NonNull final AlarmRecViewHolder holder, final int position) {
+
+        // pass currentItem to ViewHolder
         final AlarmEntity currentItem = mAlarmDataList.get(position);
-        final AlarmHelper ah = new AlarmHelper();
-
-        // Gets time in format from milliSeconds to format: 08:30 PM
-        // and its properly ordered in recyclerView!
-        final long alarmTimeInMillis = currentItem.getAlarmTime();
-        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm aa",
-                Locale.getDefault());
-        final String formatted = sdf.format(alarmTimeInMillis);
-
-        holder.tvAlarmTime.setText(formatted);
-        holder.switchAlarmEnabled.setChecked(currentItem.getAlarmEnabled());
-
-        // Enable / Disable Alarm on switch toggle
-        holder.switchAlarmEnabled.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!holder.switchAlarmEnabled.isChecked()) {
-                    ah.cancelAlarm(currentItem.getAlarmId(), false);
-                    Snackbar.make(view, "Alarm for " + formatted + " disabled", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                } else {
-                    ah.oldAlarmId = currentItem.getAlarmId();
-                    ah.reEnableAlarm(alarmTimeInMillis);
-                    Snackbar.make(view, "Alarm Set for " + formatted, Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            }
-        });
-
-        // Delete Alarm on ImageButton Click
-        holder.ibAlarmDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                int alarmIdOnPos = currentItem.getAlarmId();
-                // pass alarm id and true to delete: if false then just disable alarm
-                ah.cancelAlarm(alarmIdOnPos, true);
-
-                Snackbar.make(view, "Alarm Deleted", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-            }
-        });
-        /* TODO: Implement way to show actual AlarmTitle
-         * TODO: Also add in AlarmEntity
-         */
+        holder.bindTo(currentItem);
     }
 
     @Override
@@ -106,18 +52,4 @@ public class AlarmRecViewAdapter extends RecyclerView.Adapter<AlarmRecViewAdapte
         return mAlarmDataList.size();
     }
 
-    public static class AlarmViewHolder extends RecyclerView.ViewHolder {
-        TextView tvAlarmTime;
-        EditText etAlarmTitle;
-        SwitchCompat switchAlarmEnabled;
-        ImageButton ibAlarmDelete;
-
-        public AlarmViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tvAlarmTime = itemView.findViewById(R.id.item_alarm_time);
-            etAlarmTitle = itemView.findViewById(R.id.item_alarm_title);
-            switchAlarmEnabled = itemView.findViewById(R.id.item_alarm_enabled);
-            ibAlarmDelete = itemView.findViewById(R.id.item_alarm_delete);
-        }
-    }
 }
