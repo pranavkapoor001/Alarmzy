6e2f77c66c21abf03f38077a7c5eaf83c081cead
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-01-07 21:00:24 +0530

    Alarmzy: Don't show missed alarm notification after reboot
    * While rescheduling we check if parent alarm time has passed
      to show missed alarm notification
    
    * This is wrong since parent alarm time is never updated
      and it can falsely notify even if parent alarm was already dismissed by user
    
    * Partial revert of: https://github.com/pranavkapoor001/Alarmzy/commit/3544404b072f66736b73515859aafb872f802987
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- app/src/main/java/com/pk/alarmzy/alarm/services/ReSchedAlarmService.java
+++ app/src/main/java/com/pk/alarmzy/alarm/services/ReSchedAlarmService.java
@@ -10,7 +10,6 @@ import androidx.core.app.JobIntentService;
 import com.pk.alarmzy.alarm.db.AlarmEntity;
 import com.pk.alarmzy.alarm.db.AlarmRepository;
 import com.pk.alarmzy.alarm.helper.AlarmHelper;
-import com.pk.alarmzy.alarm.helper.NotificationHelper;
 import com.pk.alarmzy.misc.DaysOfWeek;
 import com.pk.alarmzy.misc.MyApplication;
 
@@ -36,8 +35,6 @@ public class ReSchedAlarmService extends JobIntentService {
         AlarmRepository ar = new AlarmRepository(MyApplication.getContext());
         List<AlarmEntity> alarms = ar.getAllAlarmsReSched();
 
-        NotificationHelper nh;
-
         // Iterate through all alarms and reEnable where getAlarmEnabled() returns true
         AlarmEntity ae;
         for (int i = 0; i < alarms.size(); i++) {
@@ -50,10 +47,6 @@ public class ReSchedAlarmService extends JobIntentService {
                 if (ae.getAlarmTime() < System.currentTimeMillis() &&
                         !ae.getDaysOfRepeatArr()[DaysOfWeek.IsRECURRING]) {
 
-                    // Deliver missed alarm notification
-                    nh = new NotificationHelper(getApplicationContext(), ae.getAlarmId());
-                    nh.deliverMissedNotification(ae.getAlarmTime());
-
                     Log.e(TAG, "onHandleWork: ParentTime passed, no child alarms");
                     ah.cancelAlarm(ae, false, true, -1);
                     continue;
@@ -64,10 +57,6 @@ public class ReSchedAlarmService extends JobIntentService {
                  * But Schedule child alarms then skip to next iteration
                  */
                 else if (ae.getAlarmTime() < System.currentTimeMillis()) {
-
-                    // Deliver missed alarm notification
-                    nh = new NotificationHelper(getApplicationContext(), ae.getAlarmId());
-                    nh.deliverMissedNotification(ae.getAlarmTime());
 
                     Log.e(TAG, "onHandleWork: ParentTime passed, but child alarms enabled");
                     ar.reEnableAlarmChild(ae.getAlarmId());
